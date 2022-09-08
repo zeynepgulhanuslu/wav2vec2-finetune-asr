b73e78eb8af7f5f9852158f6d531ddb6dd164530
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2022-09-08 14:37:17 +0300

    some changes

--- training/finetune_with_flash.py
+++ training/finetune_with_flash.py
@@ -12,19 +12,21 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, required=True, help='epoch count')
     parser.add_argument('--num_nodes', type=int, required=True, help='num nodes')
     parser.add_argument('--gpus', type=int, required=True, help='gpu counts')
+    parser.add_argument('--batch_size', type=int, required=True, help='batch size')
     parser.add_argument('--model_file', type=str, required=True, help='output model file')
 
     args = parser.parse_args()
-    train_json_file = args.train
-    test_json_file = args.test
+    train_csv_file = args.train
+    test_csv_file = args.test
     model_file = args.model_file
+    batch_size = args.batch_size
 
-    datamodule = SpeechRecognitionData.from_json(
+    datamodule = SpeechRecognitionData.from_csv(
         input_field="path",
         target_field="sentence",
-        train_file=train_json_file,
-        test_file=test_json_file,
-        batch_size=4)
+        train_file=train_csv_file,
+        test_file=test_csv_file,
+        batch_size=batch_size)
 
     model = SpeechRecognition(backbone="patrickvonplaten/wav2vec2-large-xls-r-300m-turkish-colab")
 
