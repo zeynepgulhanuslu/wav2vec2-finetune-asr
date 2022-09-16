c4ca2a8629ed555ba37a3440d479ead08b560a0e
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2022-09-16 13:07:34 +0300

    force_alignment.py tried

--- inference/force_alignment.py
+++ inference/force_alignment.py
@@ -1,36 +1,41 @@
+import argparse
+
 import torch
 from datasets import load_dataset, Audio
 from transformers import AutoProcessor, AutoModelForCTC
 
+from dataloader.convert_kaldi_data import get_dataset
 from training.finetune_with_hg import remove_special_characters, replace_hatted_characters, prepare_dataset
 
 if __name__ == '__main__':
     model_path = "patrickvonplaten/wav2vec2-large-xls-r-300m-turkish-colab"
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('--test', type=str, required=True, help='test csv data file')
+    args = parser.parse_args()
+    test_file = args.test
     processor = AutoProcessor.from_pretrained(model_path)
     model = AutoModelForCTC.from_pretrained(model_path)
 
     model = model.to('cpu')
     print(model)
-    common_voice_test = load_dataset("common_voice", "tr", split="test")
-    common_voice_test = common_voice_test.remove_columns(
-        ["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
-    common_voice_test = common_voice_test.map(remove_special_characters)
-    common_voice_test = common_voice_test.map(replace_hatted_characters)
-    common_voice_test = common_voice_test.cast_column("audio", Audio(sampling_rate=16_000))
-    common_voice_test = common_voice_test.map(prepare_dataset, remove_columns=common_voice_test.column_names,
+    test_dataset = get_dataset(test_file)
+
+    test_dataset = test_dataset.map(remove_special_characters)
+    test_dataset = test_dataset.map(replace_hatted_characters)
+    test_dataset = test_dataset.cast_column("audio", Audio(sampling_rate=16_000))
+    test_dataset = test_dataset.map(prepare_dataset, remove_columns=test_dataset.column_names,
                                               num_proc=4, keep_in_memory=True)
 
-    input_dict = processor(common_voice_test[0]["input_values"], return_tensors="pt", padding=True)
+    input_dict = processor(test_dataset[0]["input_values"], return_tensors="pt", padding=True)
 
     logits = model(input_dict.input_values.to("cpu")).logits
 
     pred_ids = torch.argmax(logits, dim=-1)[0]
 
-    common_voice_test_transcription = load_dataset("common_voice", "tr", data_dir="./cv-corpus-6.1-2020-12-11",
-                                                   split="test")
+
     print("Prediction:")
     print(processor.decode(pred_ids))
 
     print("\nReference:")
-    print(common_voice_test_transcription[0]["sentence"].lower())
+    print(test_dataset[0]["sentence"].lower())
